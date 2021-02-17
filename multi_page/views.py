1998373bc1b90313bf0bfa1485ac284194cba58e
Hercules Du Preez <hdupreez@iress.co.za>
2021-02-17 16:14:25 +0200

    This is a first test version of Django

---
+++ multi_page/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'index.html')
+
+def contact(request):
+    user_input = request.GET['user_input']
+    return render(request,'contact.html', {'home_input':user_input})
